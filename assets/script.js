c02896a0b4c7275b9fa6a37ffcfd5055b804451b
amachkel <amachkel@gmail.com>
2022-02-17 19:34:32 -0500

    Computer choice has .5 sec delay and displays with red background.

--- assets/script.js
+++ assets/script.js
@@ -9,27 +9,43 @@ renderChoiceBtns = () => {
   paperEl.css("display", "block");
 
   $(".choice").click(function () {
-    $(".choice").each(function() {
+    $(".choice").each(function () {
       $(this).css("background", "white");
-    })
+    });
     let userChoice = $(this).attr("value");
-   $(this).css("background", "blue");
+    $(this).css("background", "blue");
     getCompChoice(userChoice);
   });
 };
 getCompChoice = (userChoice) => {
   let choiceArray = ["rock", "paper", "scissors"];
   let compChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
-  console.log(compChoice);
+  // let compChoice = $(".comp-choice");
+  setTimeout(function () {
+    console.log(compChoice);
+    const compScissorsEl = $("#scissors2");
+    const compRockEl = $("#rock2");
+    const compPaperEl = $("#paper2");
+    if (compChoice === compScissorsEl.value) {
+      compScissorsEl.css("display", "block");
+    } else if (compChoice === compRockEl) {
+      compRockEl.css("display", "block");
+    } else {
+      compPaperEl.css("display", "block");
+    }
+  }, 500);
   getWinner(userChoice, compChoice);
-}
+};
 getWinner = (userChoice, compChoice) => {
-if (userChoice === compChoice) {
-  console.log("It's a tie!");
-} else if ((userChoice === "rock" && compChoice === "scissors") || (userChoice === "scissors" && compChoice === "paper")) {
-console.log("Player wins!");
-} else {
-  console.log("Computer wins!");
-}
-}
+  if (userChoice === compChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (userChoice === "rock" && compChoice === "scissors") ||
+    (userChoice === "scissors" && compChoice === "paper")
+  ) {
+    console.log("Player wins!");
+  } else {
+    console.log("Computer wins!");
+  }
+};
 startGame();
