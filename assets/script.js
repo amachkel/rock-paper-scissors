d22b1db24ff44003223de207bd074299057b4b58
amachkel <amachkel@gmail.com>
2022-02-17 22:16:44 -0500

    working out issues with modal

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,7 @@ function renderChoiceBtns() {
     $(this).css("background", "blue");
     getCompChoice(userChoice);
   });
+  return;
 }
 
 getCompChoice = (userChoice) => {
@@ -34,21 +35,40 @@ getCompChoice = (userChoice) => {
   }, 1000);
 };
 getWinner = (userChoice, compChoice) => {
+  let wins = 0;
+  let losses = 0;
+  let ties = 0;
   let winStatusEl = $("<p>");
+  let scoresEl = $("<p>");
   const myModal = $(".modal-body");
   myModal.append(winStatusEl);
+  myModal.append(scoresEl);
   setTimeout(function () {
-    $('#my-modal').modal('show');
     if (userChoice === compChoice) {
+      ties++;
       winStatusEl.text("It's a tie!");
     } else if (
       (userChoice === "rock" && compChoice === "scissors") ||
       (userChoice === "scissors" && compChoice === "paper")
     ) {
+      wins++;
       winStatusEl.text("Player wins!");
     } else {
+      losses++;
       winStatusEl.text("Computer wins!");
     }
   }, 1000);
-  
+  setTimeout(function () {
+    $("#my-modal").modal("show");
+    scoresEl.text(`Wins: ${wins} \n Losses: ${losses} \n Ties: ${ties}`);
+  }, 1000);
+  return;
 };
+
+$("#clearBtn").click(function () {
+  location.reload();
+});
+
+$("#playAgain").click(function () {
+  renderChoiceBtns();
+});
